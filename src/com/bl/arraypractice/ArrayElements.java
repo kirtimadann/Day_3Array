e1d04221b98395d4b0280f1f99848070c1211db1
lunainfotec <kirtimadan1997@gmail.com>
2023-03-29 23:07:40 +0530

    added : code to print array elements

---
+++ src/com/bl/arraypractice/ArrayElements.java
@@ -0,0 +1,12 @@
+package com.bl.arraypractice;
+
+public class ArrayElements {
+    public static void main(String[] args) {
+        int [] arr = new int []{4,6,8,3,6,4,9,7};
+
+        for (int i=0; i<arr.length; i++){
+
+            System.out.println(arr[i]);
+        }
+    }
+}
